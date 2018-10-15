d216a25e10d964bd3df0621ba42b7784004de83b
shadow0403bsr <baljindersingh.becse16@pec.edu.in>
2018-10-15 20:25:08 +0530

    Update PriceCrawler.py
    Minor code changes

--- PriceCrawler.py
+++ PriceCrawler.py
@@ -8,93 +8,93 @@ import codecs
 #image = Image.open('local-filename.png')
 #image.show()
 
-key = input()
+query = input()
 
-key_amzn = key.replace(" ", "+")
-len_amzn = len(key_amzn)
+key_a = query.replace(" ", "+")
+len_a = len(key_a)
 key_amazon = ""
-product_title = []
-product_url = []
-product_curprice = []
-product_orgprice = []
-for lc in range(0,len_amzn):
-    if(re.match(r'[^a-zA-Z0-9+]',key_amzn[lc],re.M|re.I)):
-        temp = codecs.encode(key_amzn[lc].encode("utf-8"), "hex")
+product_title_amazon = []
+product_url_amazon = []
+product_curprice_amazon = []
+product_orgprice_amazon = []
+for lc in range(0, len_a):
+    if(re.match(r'[^a-zA-Z0-9+]',key_a[lc],re.M|re.I)):
+        temp = codecs.encode(key_a[lc].encode("utf-8"), "hex")
         key_amazon += "%" + temp.decode("utf-8")
     else:
-        key_amazon += key_amzn[lc]
-url_amzn = 'https://www.amazon.in/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=' + key_amazon
-print(url_amzn)
+        key_amazon += key_a[lc]
+url_amazon = 'https://www.amazon.in/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=' + key_amazon
+print(url_amazon)
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-source_code_amzn = urlopen(Request(url_amzn, headers=headers))
-soup_amzn = BeautifulSoup(source_code_amzn, "html.parser")
+source_code_amazon = urlopen(Request(url_amazon, headers=headers))
+soup_amazon = BeautifulSoup(source_code_amazon, "html.parser")
 
 
-for titles in soup_amzn.find_all('a', {'class': 'a-link-normal s-access-detail-page s-color-twister-title-link a-text-normal'}):
+for titles in soup_amazon.find_all('a', {'class': 'a-link-normal s-access-detail-page s-color-twister-title-link a-text-normal'}):
     try:
-        product_title.append(titles.get('title'))
+        product_title_amazon.append(titles.get('title'))
         if(re.match("/gp",titles.get('href'))):
-            product_url.append('https://www.amazon.in/'+titles.get('href'))
+            product_url_amazon.append('https://www.amazon.in/'+titles.get('href'))
         else:
-            product_url.append(titles.get('href'))
+            product_url_amazon.append(titles.get('href'))
     except AttributeError:
         continue
 cnt = 0
-for titles in soup_amzn.find_all('div', {'class': 'a-column a-span7'}):
+for titles in soup_amazon.find_all('div', {'class': 'a-column a-span7'}):
     temp = ""
     check = titles.find_all('span', {'class': 'a-size-base a-color-price s-price a-text-bold'})
     if(check == []):
-        product_curprice.append('Currently Not Available')
-        product_orgprice.append('Currently Not Available')
+        product_curprice_amazon.append('Currently Not Available')
+        product_orgprice_amazon.append('Currently Not Available')
         continue
     for divs in titles.find_all('span', {'class': 'a-size-base a-color-price s-price a-text-bold'}):
         temp = divs.get_text()
-        product_curprice.append(divs.get_text())
+        product_curprice_amazon.append(divs.get_text())
         break
     check = titles.find_all('span', {'class': 'a-size-small a-color-secondary a-text-strike'})
-    if(check == []):
-        product_orgprice.append(temp)
+    if (check == []):
+        product_orgprice_amazon.append(temp)
     for divs in titles.find_all('span', {'class': 'a-size-small a-color-secondary a-text-strike'}):
-        product_orgprice.append(divs.get_text())
+        product_orgprice_amazon.append(divs.get_text())
         break
     cnt = cnt + 1
-    if (cnt == len(product_title)):
+    if (cnt == len(product_title_amazon)):
         break
-print(len(product_title),len(product_url),len(product_curprice),len(product_orgprice))
-for lc in range(0, len(product_curprice)):
-    print(product_title[lc] + "\n" + product_url[lc] + "\n" + product_curprice[lc] + "\n" + product_orgprice[lc] + "\n")
+print(len(product_title_amazon), len(product_url_amazon), len(product_curprice_amazon), len(product_orgprice_amazon))
+for lc in range(0, len(product_curprice_amazon)):
+    print(product_title_amazon[lc] + "\n" + product_url_amazon[lc] + "\n" + product_curprice_amazon[lc] + "\n" + product_orgprice_amazon[lc] + "\n")
 
-key_fpkt = key
-len_fpkt = len(key_fpkt)
+key_f = query
+len_f = len(key_f)
 key_flipkart = ""
 product_title_flipkart = []
 product_url_flipkart = []
 product_curprice_flipkart = []
 product_orgprice_flipkart = []
-for lc in range(0, len_fpkt):
-    if(re.match(r'[^a-zA-Z0-9]', key_fpkt[lc], re.M|re.I)):
-        temp = codecs.encode(key_fpkt[lc].encode("utf-8"), "hex")
+for lc in range(0, len_f):
+    if(re.match(r'[^a-zA-Z0-9]', key_f[lc], re.M|re.I)):
+        temp = codecs.encode(key_f[lc].encode("utf-8"), "hex")
         key_flipkart += "%" + temp.decode("utf-8")
     else:
-        key_flipkart += key_fpkt[lc]
-url_fpkt = 'https://www.flipkart.com/search?q='+key_flipkart+'&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off'
-print(url_fpkt)
-source_code_fpkt = urlopen(Request(url_fpkt, headers=headers))
-soup_fpkt = BeautifulSoup(source_code_fpkt, "html.parser")
+        key_flipkart += key_f[lc]
+url_flipkart = 'https://www.flipkart.com/search?q='+key_flipkart+'&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off'
+print(url_flipkart)
+source_code_flipkart = urlopen(Request(url_flipkart, headers=headers))
+soup_flipkart = BeautifulSoup(source_code_flipkart, "html.parser")
 
-if(soup_fpkt.find_all('a', {'class': '_2cLu-l'}) == []):
-    for titles in soup_fpkt.find_all('div', {'class': '_3wU53n'}):
+if(soup_flipkart.find_all('a', {'class': '_2cLu-l'}) == []):
+    for titles in soup_flipkart.find_all('div', {'class': '_3wU53n'}):
         try:
             product_title_flipkart.append(titles.get_text())
         except AttributeError:
             continue
-    for titles in soup_fpkt.find_all('a', {'class': '_31qSD5'}):
+    for titles in soup_flipkart.find_all('a', {'class': '_31qSD5'}):
         try:
             product_url_flipkart.append('https://www.flipkart.com' + titles.get('href'))
         except AttributeError:
             continue
 else:
-    for titles in soup_fpkt.find_all('a', {'class': '_2cLu-l'}):
+    for titles in soup_flipkart.find_all('a', {'class': '_2cLu-l'}):
         try:
             product_title_flipkart.append(titles.get('title'))
             product_url_flipkart.append('https://www.flipkart.com' + titles.get('href'))
@@ -103,7 +103,7 @@ else:
 
 
 cnt = 0
-for titles in soup_fpkt.find_all('div', {'class': '_1uv9Cb'}):
+for titles in soup_flipkart.find_all('div', {'class': '_1uv9Cb'}):
     temp = ""
     for div in titles.find_all('div', {'class': '_1vC4OE'}):
         temp = div.get_text()
